ff4a407a8273bcbb3adbf4770b287f9368af140d
asamiyatsuki <guang15@vt.edu>
2017-02-02 21:14:43 -0500

    Rewite parser, add more functions

--- src/dice.js
+++ src/dice.js
@@ -1,45 +1,162 @@
+//格式：.rd[空格][你要投掷的内容]
+
+//var Blaysiel = {
+//    input: ""
+//}
+//Blaysiel.input = ".rd 3d20 +99d100- 900 +73随便丢";
+// Used for test purpose, omitted.
+
+var diceInput = Blaysiel.input;
+
+// Basic dice class.
+function Dice(type) {
+    this.type = type;
+}
+
+// .rd type dice
+function RdDice(type, sides) {
+    Dice.call(this, type);
+    this.sides = sides;
+}
+
+RdDice.prototype.roll = function() {
+    return Math.floor(Math.random() * this.sides + 1);
+} // end of the class
+
 // entrance here
-function diceParse(entry)
-{
-    if (typeof entry == "string") {
-        if (entry.match(/^.rd/))
-        {
-            var parsed = entry.match(/^\s*.[a-z][a-z]\s\d+d\d+(\s*\+\s*\d+d\d+|\s*\+\s*\d+)*/);
-            if (parsed) {
-                return rdRoll(parsed[0]);
+function diceParse(entry) {
+    diceType = entry.slice(0, 3);
+    diceContent = entry.substring(3);
+    output = "";
+    switch (diceType) {
+        case ".rd": // Normal rolls
+            result = rdParse(diceContent);
+            if (result) {
+                return rdPrinter(result);
             }
-            return false;
+            else {
+                return "";
+            }
+        default:
+            return "";
+    }
+}
+
+// Parser of rd rolls
+function rdParse(entry) {
+    syntax = entry.match(/^\s\d{0,2}d\d{0,3}((\s*(\+|-)\s*\d{0,2}d\d{0,3})|(\s*(\+|-)\s*\d{0,3}))*/g);
+    if (syntax) { // syntax check
+        var comment = entry.substring(syntax[0].length);
+        var sum = 0
+        var bonus = 0;
+        var resultCollection = [];
+        var diceChunks = syntax[0].match(/(^\s*\d+d\d+)|((\+|-)(\s*\d+d\d+))|((\+|-)\s*\d+)/g);
+        if (diceChunks.length > 10) // drop too long expressions
+        {
+            return "";
         }
-        return false;
-        //need to add more cases, placeholder
+        for (var i = 0; i < diceChunks.length; i++) {
+            if (diceChunks[i].charAt(0) === "+")
+            {
+                if (diceChunks[i].match(/\d+d\d+/g))
+                {
+                    var amountAndSides = diceChunks[i].match(/\d+/g);
+                    results = rdRollDices(parseInt(amountAndSides[0]),parseInt(amountAndSides[1]));
+                    for (i2 = 0; i2 < results.length; i2++) {
+                        sum += results[i2];
+                    }
+                    resultCollection = resultCollection.concat(results);
+                }
+                else {
+                    bonusVal = parseInt(diceChunks[i].match(/\d+/g));
+                    bonus += bonusVal;
+                    sum += bonusVal;
+                }
+            }
+            else if (diceChunks[i].charAt(0) === "-") {
+                if (diceChunks[i].match(/\d+d\d+/g))
+                {
+                    var amountAndSides = diceChunks[i].match(/\d+/g);
+                    results = rdRollDices(amountAndSides[0], amountAndSides[1]);
+                    for (var i3 = 0; i3 < results.length; i3++) {
+                        sum -= results[i3];
+                    }
+                    resultCollection = resultCollection.concat(results);
+                }
+                else {                                          // if not + or -
+                    bonusVal = parseInt(diceChunks[i].match(/\d+/g));
+                    bonus -= bonusVal;
+                    sum -= bonusVal;
+                }
+            }
+            else {
+                var amountAndSides = diceChunks[i].match(/\d+/g);
+                results = rdRollDices(parseInt(amountAndSides[0]),parseInt(amountAndSides[1]));
+                for (i4 = 0; i4 < results.length; i4++) {
+                    sum += results[i4];
+                }
+                resultCollection = resultCollection.concat(results);
+            }
+        }
+        var output = {
+            sum: sum,
+            bonus: bonus,
+            comment: comment,
+            resultCollection: resultCollection
+        };
+        return output;
+    }
+    return "";
+}
+
+// rdParse helper
+function rdRollDices(amount, sides) {
+    var results = [];
+    for (var i = 0; i < amount; i++) {
+        results.push(new RdDice("rd", sides).roll());
+    };
+    return results;
+}
+
+function rdPrinter(entry)
+{
+    var output = ""
+    if (entry.comment) {
+        output += "因为：";
+        output += entry.comment + "，";
+    }
+    output += "骰机发出了奇怪的响声，随后吐出了："
+    if (entry.resultCollection.length >= 50)
+    {
+        for (var i = 0; i < 50; i++) {
+            output += entry.resultCollection[i] + ", ";
+        }
+        output += "......你丢的骰子太多了！骰机懒得全部列出。你的总加值为：";
+        output += entry.bonus;
+        output += "，点数总和为：";
+        output += entry.sum + "。";
     }
     else {
-        return false;
-    }
-}
-
-// Normal rolls, results are automatically accumulated.
-function rdRoll(entry) {
-    var sum = 0;
-    var chunks = entry.split("+");
-    for (var i = 0; i < chunks.length; i++) {
-        if (chunks[i].match(/\s*\d+d\d+\s*/)) {
-            sum += rdDiceRanGen(chunks[i]);
+        for (var j = 0; j < entry.resultCollection.length; j++) {
+            output += entry.resultCollection[j] + ", ";
+        }
+        if (entry.bonus) {
+            output += "你的总加值为：";
+            output += entry.bonus;
+            output += "，点数总和为：";
         }
         else {
-            sum += parseInt(chunks[i]);
+            output += "点数总和为：";
         }
+        output += entry.sum + "。";
     }
-    return sum;
+    return output;
 }
 
-// Random number generator for .rd rolls.
-function rdDiceRanGen(entry) {
-    var results = entry.match(/\d+/g);
-    var amount = parseInt(results[0]);
-    var type = parseInt(results[1]);
-    return amount * Math.floor(Math.random() * type + 1);
+var Iturya = {
+    output: ""
 }
 
-//Tests
-//console.log(diceParse(".rd 3d6 + 1d20 + 3 Here's some comments."));
+Iturya.output = diceParse(diceInput);
+
+// console.log(Iturya.output);
